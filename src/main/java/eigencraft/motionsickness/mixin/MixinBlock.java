218ef90b715410d38817bb89007674f32ae2ad65
recursive_tree <recursivetreemail@gmail.com>
2020-07-01 16:53:42 +0200

    fixed it

--- src/main/java/eigencraft/motionsickness/mixin/MixinBlock.java
+++ src/main/java/eigencraft/motionsickness/mixin/MixinBlock.java
@@ -1,26 +1,19 @@
 package eigencraft.motionsickness.mixin;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public abstract class MixinBlock {
-    @Shadow @Final protected float slipperiness;
+
 
     @Inject(method = "getSlipperiness", at = @At("HEAD"), cancellable = true)
     public void setCustomSlipperiness(CallbackInfoReturnable<Float> cir) {
-        float custom = 100f; // TODO I have no idea how configs are handled in here (bagatelle)
-        if(custom != this.slipperiness) {
-            cir.setReturnValue(custom);
-            cir.cancel();
-        }
+        float custom = 0.99f; // TODO I have no idea how configs are handled in here (bagatelle)
+        cir.setReturnValue(custom);
+        cir.cancel();
     }
 }
