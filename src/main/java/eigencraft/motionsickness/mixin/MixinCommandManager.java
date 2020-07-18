a8abb4bb38f9d791402d44b3dd08bb2759c23675
recursive_tree <recursivetreemail@gmail.com>
2020-07-18 09:39:34 +0200

    cleaner command

--- src/main/java/eigencraft/motionsickness/mixin/MixinCommandManager.java
+++ src/main/java/eigencraft/motionsickness/mixin/MixinCommandManager.java
@@ -1,11 +1,12 @@
 package eigencraft.motionsickness.mixin;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.tree.LiteralCommandNode;
-import eigencraft.motionsickness.SetSlipperinessCommand;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.tree.ArgumentCommandNode;
+import eigencraft.motionsickness.Settings;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,9 +18,17 @@ public class MixinCommandManager {
     private void onInit(CommandManager.RegistrationEnvironment environment, CallbackInfo ci) {
         CommandDispatcher<net.minecraft.server.command.ServerCommandSource> dispatcher = ((CommandManager) (Object) this).getDispatcher();
 
+        ArgumentCommandNode<ServerCommandSource, Float> argInterval = CommandManager.argument("slipperiness", FloatArgumentType.floatArg(0,1.0f))
+                .executes(c -> {
+                    Settings.slipperiness = FloatArgumentType.getFloat(c, "slipperiness");
+                    c.getSource().sendFeedback(new LiteralText(String.format("Set slipperiness to %f",Settings.slipperiness)),false);
+                    return 1;
+                }).build();
+
         dispatcher.register(
-                CommandManager.literal("motion-slipperiness")
-                        .then(SetSlipperinessCommand.registerSubCommand(dispatcher))
+                CommandManager.literal("motion-slipperiness").then(argInterval)
+                        //.then(SetSlipperinessCommand.registerSubCommand(dispatcher))
         );
+
     }
 }
